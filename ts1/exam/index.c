97dd96e80cc82c16d03792238d65e9b4d44ed2d3
unknown <fadikbb2004t@gmail.com>
2023-12-11 15:08:51 +0200

    new

--- ts1/exam/index.c
+++ ts1/exam/index.c
@@ -1,69 +1,103 @@
 #include <stdio.h>
 int main()
 {
-    float salary, bouns, b_salary, sum, tax;
-    int salary_ch, nb_ch, workedHour;
-    char status;
-    printf("Enter your total worked hour per Month: ");
-    scanf("%d", &workedHour);
-    printf("Enter your salary: ");
-    scanf("%f", &salary);
-    printf("Are you married? (yes:y/no:n): ");
-    scanf(" %c", &status);
+    // float salary, bouns, b_salary, sum, tax;
+    // int salary_ch, nb_ch, workedHour;
+    // char status;
+    // printf("Enter your total worked hour per Month: ");
+    // scanf("%d", &workedHour);
+    // printf("Enter your salary: ");
+    // scanf("%f", &salary);
+    // printf("Are you married? (yes:y/no:n): ");
+    // scanf(" %c", &status);
 
-    b_salary = workedHour * salary;
-    if (workedHour < 100)
-    {
-        bouns = 0;
-    }
-    else if (workedHour >= 100 && workedHour <= 150)
-    {
-        bouns = ((50 * 0.03) + (50 * 0.04) + ((workedHour - 100) * 0.05));
-    }
-    else
-    {
-        bouns = workedHour * 0.065;
-    }
-    if (status == 'y' || status == 'Y')
-    {
-        printf("How many children do you have? ");
-        scanf("%d", &nb_ch);
-        status = 100;
-        if (nb_ch <= 3)
-        {
-            salary_ch = nb_ch * 40;
-        }
-        else
-        {
-            salary_ch = 3 * 40;
-            salary_ch = salary_ch + ((nb_ch - 3) * 5);
-        }
-    }
-    else if (status != 'y' || status != 'Y')
-    {
-        status = 0;
-        nb_ch = 0;
-    }
+    // b_salary = workedHour * salary;
+    // if (workedHour < 100)
+    // {
+    //     bouns = 0;
+    // }
+    // else if (workedHour >= 100 && workedHour <= 150)
+    // {
+    //     bouns = ((50 * 0.03) + (50 * 0.04) + ((workedHour - 100) * 0.05));
+    // }
+    // else
+    // {
+    //     bouns = workedHour * 0.065;
+    // }
+    // if (status == 'y' || status == 'Y')
+    // {
+    //     printf("How many children do you have? ");
+    //     scanf("%d", &nb_ch);
+    //     status = 100;
+    //     if (nb_ch <= 3)
+    //     {
+    //         salary_ch = nb_ch * 40;
+    //     }
+    //     else
+    //     {
+    //         salary_ch = 3 * 40;
+    //         salary_ch = salary_ch + ((nb_ch - 3) * 5);
+    //     }
+    // }
+    // else if (status != 'y' || status != 'Y')
+    // {
+    //     status = 0;
+    //     nb_ch = 0;
+    // }
+    // if (b_salary < 1200)
+    // {
+    //     tax = b_salary * 0.03;
+    // }
+    // else if (b_salary >= 1200 && b_salary <= 2000)
+    // {
+    //     tax = (b_salary * 0.07) + ((salary_ch + status) * 0.03);
+    // }
+    // else
+    // {
+    //     tax = b_salary * 0.15;
+    // }
+    // sum = (float)(b_salary - tax) + bouns + (float)(status + salary_ch);
+    // printf("salary in USD is %.1f $ \n", sum);
+    // sum = sum * 90000;
+    // printf("salary in LPB is %d L.L. ", (int)sum);
+    //-----------------------------------------------------------------------------
+    //     int n, s, sum;
+    //     printf("enter an integer: ");
+    //     scanf("%d", &n);
+    //     printf("enter a sum: ");
+    //     scanf("%d", &s);
+    //     n += n;
+    //     if (n > s)
+    //     {        sum =n - s;
+    //         printf("you need to subtracted %d to reach the sum.", sum);
 
-    {
-        nb_ch = 0;
-        status = 0;
-    }
+    //     }
+    //     else
+    //     {
+    //   sum = s -n;
+    //         printf("you need to add %d to reach the sum.", sum);
+    //     }
+    //--------------------------------------------------------------------------------
+    // char color;
+    // int id;
+    // printf("Enter your id: ");
+    // scanf("%d",&id);
+    // printf("Enter your eye color: ");
+    // scanf(" %c",&color);
+    // if (color=='b'||color=='B')
+    // {
+    //     printf("your Soul is warm and open");
+    // }
+    // else if (color=='g'||color=='G')
+    // {
+    //     printf("your soul knows what it wants ");
+    // }
+    // else if (color=='u'||color=='U')
+    // {
+    //     printf("your soul is wise");
+    // }else if (color>='a' &&color<='z'||color>='A' &&color<='Z'){
+    //     printf("your soul is filled with curiosity"); 
+    // }else
+    // printf("invaled input");
 
-    if (b_salary < 1200)
-    {
-        tax = b_salary * 0.03;
-    }
-    else if (b_salary >= 1200 && b_salary <= 2000)
-    {
-        tax = (b_salary * 0.07) + ((salary_ch + status) * 0.03);
-    }
-    else
-    {
-        tax = b_salary * 0.15;
-    }
-    sum = (float)(b_salary - tax) + bouns + (float)(status + salary_ch);
-    printf("salary in USD is %.1f $ \n", sum);
-    sum = sum * 90000;
-    printf("salary in LPB is %d L.L. ", (int)sum);
 }
