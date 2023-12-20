bc691e58c551bd7b2c37f4486b2113d401c16675
unknown <fadikbb2004t@gmail.com>
2023-12-20 04:14:16 +0200

    i move forward

--- fornt-end mentor/multi-step-form-main/index.js
+++ fornt-end mentor/multi-step-form-main/index.js
@@ -71,7 +71,6 @@ input4.addEventListener("click", function () {
   for (let i = 0; i < 3; i++) {
     let price = document.querySelectorAll(".box-price")[i];
     let price1 = document.querySelectorAll(".price")[i];
-
     if (year.classList.contains("color1-2")) {
       price.innerHTML = `${10 * (i + 1)}\$/yr`;
       price1.innerHTML = `${10 * parseInt(price1.innerHTML)}\$/yr`;
@@ -100,22 +99,25 @@ inputBox[2].onclick = function () {
 };
 
 //-----------------------------------------------------------
-
-let add = document.createElement("div");
-add.className = "add";
+let sum = 0;
+let hbc4 = document.createElement("div");
+hbc4.className = "content4";
+let bc4 = document.createElement("div");
 submit[2].addEventListener("click", function () {
   for (let i = 0; i < 3; i++) {
     let text = document.querySelectorAll(".text1")[i].cloneNode(true);
     let price = document.querySelectorAll(".box-price")[i].cloneNode(true);
-    let bc4 = document.createElement("div");
     bc4.className = "cb4";
     if (contentBox[i].classList.contains("color2")) {
       text = text.childNodes[1];
       text.className = "text_copy";
       price.className = "price_copy";
+      sum += parseInt(price.innerHTML);
+      console.log(sum);
       bc4.append(text);
       bc4.append(price);
-      container4.append(bc4)
+      hbc4.append(bc4);
+      container4.append(hbc4);
       console.log(bc4);
       console.log(container4);
     }
@@ -128,24 +130,29 @@ back[2].addEventListener("click", function () {
   for (let i = 0; i < del.length; i++) {
     del[i].remove();
     del1[i].remove();
+    bc4.remove();
+    sum = 0;
   }
 });
 //-----------------------------------------------------
-
+let sum1 = 0;
+let hc4 = document.createElement("div");
 submit[1].addEventListener("click", function () {
   for (let i = 0; i < 3; i++) {
     if (cardContent[i].classList.contains("color")) {
       let cardName = document.querySelectorAll(".name-card")[i].cloneNode(true);
       let cardPrice = document.querySelectorAll(".price")[i].cloneNode(true);
-      let hc4 = document.createElement("div");
       hc4.className = "hc4";
       cardName.className = "name-card-copy";
       cardPrice.className = "price-card-copy";
+      sum1 = parseInt(cardPrice.innerHTML);
+      console.log(sum1);
       console.log(cardName);
       console.log(cardPrice);
       hc4.appendChild(cardName);
       hc4.appendChild(cardPrice);
-      container4.append(hc4);
+      hbc4.append(hc4);
+      container4.append(hbc4);
       console.log(hc4);
       console.log(container4);
     }
@@ -158,4 +165,24 @@ back[1].addEventListener("click", function () {
   let cardPrice = document.querySelector(".price-card-copy");
   cardName.remove();
   cardPrice.remove();
+  hc4.remove();
 });
+submit[2].addEventListener("click", function () {
+  let total = document.querySelector(".Total");
+  let addtional = document.createElement("div");
+  console.log(total.before(hbc4));
+  console.log(container4);
+  if (year.classList.contains("color1-2")) {
+    sum = `${sum + sum1}\$/yr`;
+  } else {
+    sum = `${sum + sum1}\$/mo`;
+  }
+  addtional.className = "sum";
+  addtional.append(sum);
+  total.append(addtional);
+});
+back[2].addEventListener("click", function () {
+  let total = document.querySelector(".sum");
+  console.log(total.remove());
+});
+
