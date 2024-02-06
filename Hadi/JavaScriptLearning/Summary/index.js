93ef377ea06247a1aac4bbd21f367031d2e73f76
unknown <fadikbb2004t@gmail.com>
2024-02-06 18:08:40 +0200

    sdfsfa

--- Hadi/JavaScriptLearning/Summary/index.js
+++ Hadi/JavaScriptLearning/Summary/index.js
@@ -1740,7 +1740,7 @@ allLis.forEach(function (ele) {
   - All In One Chain
 
 */
-let as =/[0-9]/ig
+let as = /[0-9]/gi;
 let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
 
 let solution = myString
@@ -1978,4 +1978,404 @@ console.log(finalObject);
 let newObject = Object.assign({}, obj1, { prop5: 5, prop6: 6 });
 
 console.log(newObject);
+//----------------------------------------------------------------
+/*
+  DOM
+  - What Is DOM
+  - DOM Selectors
+  --- Find Element By ID
+  --- Find Element By Tag Name
+  --- Find Element By Class Name
+  --- Find Element By CSS Selectors
+  --- Find Element By Collection
+  ------ title
+  ------ body
+  ------ images
+  ------ forms
+  ------ links
+*/
+
+let myIdElement = document.getElementById("my-div");
+let myTagElements = document.getElementsByTagName("p");
+let myClassElement = document.getElementsByClassName("my-span");
+let myQueryElement = document.querySelector(".my-span");
+let myQueryAllElement = document.querySelectorAll(".my-span");
+
+console.log(myIdElement);
+console.log(myTagElements[1]);
+console.log(myClassElement[1]);
+console.log(myQueryElement);
+console.log(myQueryAllElement[1]);
+
+console.log(document.title);
+console.log(document.body);
+console.log(document.forms[0].one.value);
+console.log(document.links[1].href);
+//----------------------------------------------------------------
+/*
+  DOM [Get / Set Elements Content And Attributes]
+  - innerHTML
+  - textContent
+  - Change Attributes Directly
+  - Change Attributes With Methods
+  --- getAttribute
+  --- setAttribute
+
+  Search
+  - innerText
+*/
+
+let myElement = document.querySelector(".js");
+
+console.log(myElement.innerHTML);
+console.log(myElement.textContent);
+
+myElement.innerHTML = "Text From <span>Main.js</span> File";
+myElement.textContent = "Text From <span>Main.js</span> File";
+
+document.images[0].src = "https://google.com";
+document.images[0].alt = "Alternate";
+document.images[0].title = "Picture";
+document.images[0].id = "pic";
+document.images[0].className = "img";
+
+let myLink = document.querySelector(".link");
+
+console.log(myLink.getAttribute("class"));
+console.log(myLink.getAttribute("href"));
+
+myLink.setAttribute("href", "https://twitter.com");
+myLink.setAttribute("title", "Twitter");
+//----------------------------------------------------------------
+/*
+  DOM [Check Attributes]
+  - Element.attributes
+  - Element.hasAttribute
+  - Element.hasAttributes
+  - Element.removeAttribute
+*/
+
+console.log(document.getElementsByTagName("p")[0].attributes);
+
+let myP = document.getElementsByTagName("p")[0];
+
+if (myP.hasAttribute("data-src")) {
+  if (myP.getAttribute("data-src") === "") {
+    myP.removeAttribute("data-src");
+  } else {
+    myP.setAttribute("data-src", "New Value");
+  }
+} else {
+  console.log(`Not Found`);
+}
+
+if (myP.hasAttributes()) {
+  console.log(`Has Attributes`);
+}
+
+if (document.getElementsByTagName("div")[0].hasAttributes()) {
+  console.log(`Has Attributes`);
+} else {
+  console.log(`Div Has No Attributes`);
+}
+//----------------------------------------------------------------
+/*
+  DOM [Create Elements]
+  - createElement
+  - createComment
+  - createTextNode
+  - createAttribute
+  - appendChild
+*/
+
+let myElement = document.createElement("div");
+let myAttr = document.createAttribute("data-custom");
+let myText = document.createTextNode("Product One");
+let myComment = document.createComment("This Is Div");
+
+myElement.className = "product";
+myElement.setAttributeNode(myAttr);
+myElement.setAttribute("data-test", "Testing");
+
+// Append Comment To Element
+myElement.appendChild(myComment);
+
+// Append Text To Element
+myElement.appendChild(myText);
+
+// Append Element To Body
+document.body.appendChild(myElement);
+//----------------------------------------------------------------
+/*
+  DOM [Create Elements]
+  - Practice Product With Heading And Paragraph
+*/
+
+let myMainElement = document.createElement("div");
+let myHeading = document.createElement("h2");
+let myParagraph = document.createElement("p");
+
+let myHeadingText = document.createTextNode("Product Title");
+let myParagraphText = document.createTextNode("Product Description");
+
+// Add Heading Text
+myHeading.appendChild(myHeadingText);
+
+// Add Heading To Main Element
+myMainElement.appendChild(myHeading);
+
+// Add Paragraph Text
+myParagraph.appendChild(myParagraphText);
+
+// Add Paragraph To Main Element
+myMainElement.appendChild(myParagraph);
+
+myMainElement.className = "product";
+
+document.body.appendChild(myMainElement);
+//----------------------------------------------------------------
+/*
+  DOM [Deal With Childrens]
+  - children
+  - childNodes
+  - firstChild
+  - lastChild
+  - firstElementChild
+  - lastElementChild
+*/
+
+let myElement = document.querySelector("div");
+
+console.log(myElement);
+console.log(myElement.children);
+console.log(myElement.children[0]);
+console.log(myElement.childNodes);
+console.log(myElement.childNodes[0]);
+
+console.log(myElement.firstChild);
+console.log(myElement.lastChild);
+
+console.log(myElement.firstElementChild);
+console.log(myElement.lastElementChild);
+//----------------------------------------------------------------
+/*
+  DOM [Events]
+  - Use Events On HTML
+  - Use Events On JS
+  --- onclick
+  --- oncontextmenu
+  --- onmouseenter
+  --- onmouseleave
+
+  --- onload
+  --- onscroll
+  --- onresize
+
+  --- onfocus
+  --- onblur
+  --- onsubmit
+*/
+
+let myBtn = document.getElementById("btn");
+
+myBtn.onmouseleave = function () {
+  console.log("Clicked");
+};
+
+window.onresize = function () {
+  console.log("Scroll");
+};
+//----------------------------------------------------------------
+/*
+  DOM [Events]
+  - Validate Form Practice
+  - Prevent Default
+*/
+
+let userInput = document.querySelector("[name='username']");
+let ageInput = document.querySelector("[name='age']");
+
+document.forms[0].onsubmit = function (e) {
+  let userValid = false;
+  let ageValid = false;
+
+  if (userInput.value !== "" && userInput.value.length <= 10) {
+    userValid = true;
+  }
+
+  if (ageInput.value !== "") {
+    ageValid = true;
+  }
+
+  if (userValid === false || ageValid === false) {
+    e.preventDefault();
+  }
+};
+
+document.links[0].onclick = function (event) {
+  console.log(event);
+  event.preventDefault();
+};
+//----------------------------------------------------------------
+/*
+  DOM [Events Simulation]
+  - click
+  - focus
+  - blur
+*/
+
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+
+window.onload = function () {
+  two.focus();
+};
+
+one.onblur = function () {
+  document.links[0].click();
+};
+//----------------------------------------------------------------
+/*
+  DOM [Class List]
+  - classList
+  --- length
+  --- contains
+  --- item(index)
+  --- add
+  --- remove
+  --- toggle
+*/
+
+let element = document.getElementById("my-div");
+
+console.log(element.classList);
+console.log(typeof element.classList);
+console.log(element.classList.contains("osama"));
+console.log(element.classList.contains("show"));
+console.log(element.classList.item("3"));
+
+element.onclick = function () {
+  element.classList.toggle("show");
+};
+//----------------------------------------------------------------
+/*
+  DOM [CSS]
+  - style
+  - cssText
+  - removeProperty(propertyName) [Inline, Stylesheet]
+  - setProperty(propertyName, value, priority)
+*/
+
+let element = document.getElementById("my-div");
+
+element.style.color = "red";
+element.style.fontWeight = "bold";
+
+element.style.cssText = "font-weight: bold; color: green; opacity: 0.9";
+
+element.style.removeProperty("color");
+element.style.setProperty("font-size", "40px", "important");
+
+document.styleSheets[0].rules[0].style.removeProperty("line-height");
+document.styleSheets[0].rules[0].style.setProperty(
+  "background-color",
+  "red",
+  "important"
+);
+//----------------------------------------------------------------
+/*
+  DOM [Deal With Elements]
+  - before [Element || String]
+  - after [Element || String]
+  - append [Element || String]
+  - prepend [Element || String]
+  - remove
+*/
+
+let element = document.getElementById("my-div");
+let createdP = document.createElement("p");
+
+// element.remove();
+//----------------------------------------------------------------
+/*
+  DOM [Traversing]
+  - nextSibling
+  - previousSibling
+  - nextElementSibling
+  - previousElementSibling
+  - parentElement
+*/
+
+let span = document.querySelector(".two");
+
+console.log(span.parentElement);
+
+span.onclick = function () {
+  span.parentElement.remove();
+};
+//----------------------------------------------------------------
+/*
+  DOM [Cloning]
+  - cloneNode(Deep)
+*/
+
+let myP1 = document.querySelector("p").cloneNode(true);
+let myDiv = document.querySelector("div");
+
+myP.id = `${myP.id}-clone`;
+
+myDiv.appendChild(myP1);
+//----------------------------------------------------------------
+/*
+  DOM [Add Event Listener]
+  - addEventListener
+  - Use Without On
+  - Attach Multiple Events
+  - Error Test
+
+  Search
+  - Capture & Bubbling JavaScript
+  - removeEventListener
+*/
+
+let myP = document.querySelector("p");
+
+// myP.onclick = one;
+// myP.onclick = two;
+
+// function one() {
+//   console.log("Message From OnClick 1");
+// }
+// function two() {
+//   console.log("Message From OnClick 2");
+// }
+
+// window.onload = "Osama";
+
+// myP.addEventListener("click", function () {
+//   console.log("Message From OnClick 1 Event");
+// });
+
+// myP.addEventListener("click", one);
+// myP.addEventListener("click", two);
+
+// myP.addEventListener("click", "String"); // Error
+
+myP.onclick = function () {
+  let newP = myP.cloneNode(true);
+  newP.className = "clone";
+  document.body.appendChild(newP);
+};
+
+// let cloned = document.querySelector(".clone"); // Error
+
+// cloned.onclick = function () {
+//   console.log("Iam Cloned");
+// };
+
+document.addEventListener("click", function (e) {
+  if (e.target.className === "clone") {
+    console.log("Iam Cloned");
+  }
+});
 //----------------------------------------------------------------
